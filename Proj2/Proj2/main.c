fe3e03dd96bfd28a3cc258cbcee0574965f6d16f
Radicitus <novivolatus@gmail.com>
2022-04-25 10:28:03 -0700

    Proj2 initial commit

---
+++ Proj2/Proj2/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 4/25/2022 10:19:46 AM
+ *  Author: camcam
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
