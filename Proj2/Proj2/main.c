8a94da32508b388c2bba461471bc937e8bb52d16
Radicitus <novivolatus@gmail.com>
2022-04-29 12:25:44 -0700

    Checkpoint

--- Proj2/Proj2/main.c
+++ Proj2/Proj2/main.c
@@ -1,6 +1,7 @@
 #include <avr/io.h>
 #include "avr.h"
 #include "lcd.h"
+#include <stdio.h>
 
 int is_pressed(int r, int c) {
 	// Set all 8 GPIOs to N/C
@@ -32,11 +33,71 @@ int get_key() {
 	return 0;
 }
 
+// Date Time
+
 typedef struct {
 	int year;
-	unsigned char month;
+	int month;
+	int day;
+	int hour;
+	int minute;
+	int second;
 	} DateTime;
 
+void advance_dt(DateTime *dt) {
+	++dt->second;
+	
+	// Second to Minute
+	if (dt->second == 60) {
+		dt->second = 0;
+		++dt->minute;
+	}
+	
+	// Minute to Hour
+	if (dt->minute == 60) {
+		dt->minute = 0;
+		++dt->hour;
+	}
+	
+	// Hour to Day
+	if (dt->hour == 24) {
+		dt->hour = 0;
+		++dt->day;
+	}
+	
+	// Day to Month
+	int days[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+	
+	// Month to Year
+	
+}
+
+void init_dt(DateTime *dt) {
+	dt->year = 2022;
+	dt->month = 12;
+	dt->day = 19;
+	dt->hour = 23;
+	dt->minute = 59;
+	dt->second = 50;
+}
+
+void print_dt(const DateTime *dt) {
+	char date[17];
+	// Print date on top row
+	lcd_pos(0,0);
+	sprintf(date, "%04d-%02d-%02d", dt->year, 
+								    dt->month, 
+								    dt->day);
+	lcd_puts2(date);
+	
+	// Print time on bottom row
+	char time[9];
+	lcd_pos(1,0);
+	sprintf(time, "%02d:%02d:%02d", dt->hour,
+								    dt->minute,
+								    dt->second);
+	lcd_puts2(time);
+}
 
 // MAIN LOOP
 
@@ -46,15 +107,15 @@ int main(void)
     SET_BIT(DDRB, 0);
     
 	// Init display
+	DateTime dt;
+	init_dt(&dt);
 	lcd_init();
 
-
-	char word[6] = "Hello";
     while (1) {
 		
 		avr_wait(1000);
-		lcd_puts2(word);
-		avr_wait(1000);
+		advance_dt(&dt);
+		print_dt(&dt);
 		
 		
 		/*
