9359a5b171d4f65eaa8eb0b14eb240ef0d50941d
Radicitus <novivolatus@gmail.com>
2022-05-04 14:05:11 -0700

    Checkpoint

--- Proj3/Proj3/avr.c
+++ Proj3/Proj3/avr.c
@@ -16,7 +16,7 @@ avr_wait(unsigned short msec)
 {
   TCCR0 = 2;
   while (msec--) {
-    TCNT0 = (unsigned char)(256 - (XTAL_FRQ / 8) * 0.001);
+    TCNT0 = (unsigned char)(256 - (XTAL_FRQ / 8) * 0.0001);
     SET_BIT(TIFR, TOV0);
     WDR();
     while (!GET_BIT(TIFR, TOV0));
--- Proj3/Proj3/main.c
+++ Proj3/Proj3/main.c
@@ -1,7 +1,7 @@
 #include "avr.h"
 
 typedef enum {
-	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+	A, As, B, C, Cs, D, Ds, E, F, Fs, G, Gs
 } Note;
 
 typedef enum {
@@ -17,19 +17,83 @@ typedef struct {
 void play_song(const PlayingNote song[], int length) {
 	int i;
 	for (i = 0; i < length; i++) {
+	}
 		play_note(&song[i]);
 	}
-}
 
 void play_note(const PlayingNote* note) {
 	int i, k;
+	int th;
+	double duration = note->duration;
 	
-	double f = (2^( note->note / 12) * 220);
+	/*
+	switch(note->duration){
+		case Wn:
+			duration = 4;
+			break;
+		case Hn:
+			duration = 2;
+			break;
+		case Qn:
+			duration = 1;
+			break;
+		case En:
+			duration = .5;
+			break;
+	}
+	*/
 	
-	float p = 1 / f;
-	float k = note->duration / p;
-	float th = p /2;
-	
+	switch(note->note){
+		case A:
+			th = 23;
+			k = duration * 220;
+			break;
+		case As:
+			th = 21;
+			k = duration * 233;
+			break;
+		case B:
+			th = 20;
+			k = duration * 245;
+			break;
+		case C:
+			th = 19;
+			k = duration * 262;
+			break;
+		case Cs:
+			th = 18;
+			k = duration * 277;
+			break;
+		case D:
+			th = 17;
+			k = duration * 294;
+			break;
+		case Ds:
+			th = 16;
+			k = duration * 311;
+			break;
+		case E:
+			th = 15;
+			k = duration * 330;
+			break;
+		case F:
+			th = 14;
+			k = duration * 349;
+			break;
+		case Fs:
+			th = 14;
+			k = duration * 370;
+			break;
+		case G:
+			th = 13;
+			k = duration * 392;
+			break;
+		case Gs:
+			th = 12;
+			k = duration * 415;
+			break;
+	}
+
 	for (i = 0; i < k; i++) {
 		SET_BIT(PORTA, 0);
 		avr_wait(th);
@@ -40,15 +104,16 @@ void play_note(const PlayingNote* note) {
 
 
 PlayingNote shooting_stars[] = {
-	{5, Wn},
-	{5, Hn},
-	{7, Hn},
-	{2, Qn},
-	{11, Qn}
+	{A, En},
+	{B, En},
+	{C, En},
+	{D, En},
+	{E, En},
+	{F, En},
+	{G, En}
 };
 
-int main(void)
-{
+main() {
 	// Set button input
 	CLR_BIT(DDRA, 1);
 	// Set LED output
@@ -61,8 +126,9 @@ int main(void)
 		// While button pushed
         if(!GET_BIT(PINA, 1)) {
 			SET_BIT(PORTB, 0);
+			play_song(shooting_stars, 7);
 		} else {
 			CLR_BIT(PORTB, 0);	
 		}
-    }
+	}
 }
