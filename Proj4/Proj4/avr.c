8b4848c544db9e7c8d1e7c3f766d2b62dc30dc8d
Radicitus <novivolatus@gmail.com>
2022-05-19 15:11:01 -0700

    Completed proj4

--- Proj4/Proj4/avr.c
+++ Proj4/Proj4/avr.c
@@ -14,9 +14,9 @@ avr_init(void)
 void
 avr_wait(unsigned short msec)
 {
-  TCCR0 = 2;
+  TCCR0 = 3;
   while (msec--) {
-    TCNT0 = (unsigned char)(256 - (XTAL_FRQ / 8) * 0.0001);
+    TCNT0 = (unsigned char)(256 - (XTAL_FRQ / 64) * 0.001);
     SET_BIT(TIFR, TOV0);
     WDR();
     while (!GET_BIT(TIFR, TOV0));
--- Proj4/Proj4/main.c
+++ Proj4/Proj4/main.c
@@ -3,32 +3,87 @@
 #include "lcd.h"
 
 
+
+float get_voltage(int sample) {
+	return (sample / 1023.0) * 5;
+}
+
 int get_sample() {
-	int result;
-	
 	ADMUX = 0b01000000;
 	ADCSRA = 0b11000000;
+	while(GET_BIT(ADCSRA, 6));
+	return ADC;
+}
+
+
+int is_pressed(int r, int c) {
+	// Set all 8 GPIOs to N/C
+	DDRC = 0;
+	PORTC = 0;
 	
+	// Set r to "0"
+	SET_BIT(DDRC, r);
+	CLR_BIT(PORTC, r);
 	
-	return result;
+	// Set c to "w1"
+	CLR_BIT(DDRC, c + 4);
+	SET_BIT(PORTC, c + 4);
+	
+	if (GET_BIT(PINC, c + 4)) {
+		return 0;
+	}
+	return 1;
+}
+
+int get_key() {
+	for (int r = 0; r < 4; r++) {
+		for (int c = 0; c < 4; c++) {
+			if (is_pressed(r, c)) {
+				return ((4 * r) + c + 1);
+			}
+		}
+	}
+	return 0;
 }
 
 
 int main(void)
 {
 	// LCD Setup
-	char display[20];
+	char display1[40];
+	char display2[40];
 	avr_init();
 	lcd_init();
 	
 	// Vars
-	int max;
-	int min;
+	float max = 0.0;
+	float min = 0.0;
+	float sum = 0.0; 
+	float count = 0.0;
+	float average = 0.0;
+	int display_vals = 0;
+	
+	// DisplayVars
+
 	
     while(1)
     {
 		
+		// Get sample
 		int new_sample = get_sample();
+		
+		
+		// Clear min/max/avg
+		if (get_key() == 1) {
+			display_vals = 0;
+		}
+		
+		// Restart min/max/avg sampling
+		if (get_key() == 2) {
+			max = 0.0, min = new_sample;
+			sum = 0.0, count = 0.0;
+			display_vals = 1;
+		}
 		
 		// Max calc
 		if (new_sample > max) {
@@ -40,10 +95,32 @@ int main(void)
 			min = new_sample;
 		}
 		
-        sprintf(display, "%.2f", (new_sample / 1023.0) * 5);
-		lcd_clr();
-		lcd_pos(0, 0);
-		lcd_puts2(display);
-		avr_wait(500);
+		// Avg calc
+		sum += new_sample;
+		count++;
+		average = sum/count;
+		
+		
+		if(display_vals == 0){
+			sprintf(display1, "V %.2f  Avg ----", get_voltage(new_sample));
+			sprintf(display2, "Mx ----  Mn ----");
+			lcd_clr();
+			lcd_pos(0, 0);
+			lcd_puts2(display1);
+			lcd_pos(1, 0);
+			lcd_puts2(display2);
+			avr_wait(500);
+		}
+		else{
+			sprintf(display1, "V %.2f Avg %.2f", get_voltage(new_sample), get_voltage(average));
+			sprintf(display2, "Mx %.2f Mn %.2f", get_voltage(max), get_voltage(min));
+			lcd_clr();
+			lcd_pos(0, 0);
+			lcd_puts2(display1);
+			lcd_pos(1, 0);
+			lcd_puts2(display2);
+			avr_wait(500);
+		}
+        
     }
 }
